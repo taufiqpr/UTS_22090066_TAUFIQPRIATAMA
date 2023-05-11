693d0bdf6920e4773d57614adb4d98c6a8ae0bcc
topikk <118111616+taufiqpr@users.noreply.github.com>
2023-05-11 19:06:41 +0700

    Add files via upload

---
+++ harber.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("#btnsearch").click(function (e) {
+      var txtInput = $("srchInv").val();
+      $("#infoinvaccr").html("Nama Anda : " + txtInput);
+      $.ajax({
+             type:"POST",
+             url: "/assets/scripts/ajax/getcurl.php",
+             data: "",
+             success: function (response) {
+                 $("#infoinvaccr").html("<br/>Response<br/> " + response)
+             }
+      })
+
+  })
+});
