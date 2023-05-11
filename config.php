5fd9f2886f3c2320684ba834bb7b72084035c896
topikk <118111616+taufiqpr@users.noreply.github.com>
2023-05-11 19:00:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('APPNAME', 'harber web');
+define('PATH', 'http://harber2c.local/');
+define('SITE_ROOT', _DIR_);
+define('MYROOT', $_SERVER['DOCUMENT_ROOT']);
+?>
---
+++ header1.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=">
+<title><?php echo APPNAME; ?></title>
+
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./sweetalert/sweetalert.css">
+
+<script src="./jQuery/jQuery-3.5.1.js"></script>
+<script src="./bootstrap/js.bootstrap.js"></script>
+<script src="./bootstrap/js/bootstrap.min.js"></script>
+<script src="./sweetalert/sweetalert.min.js"></script>
+?>
